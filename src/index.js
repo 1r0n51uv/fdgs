f93725a2d6014a214521ad2bada1b4af89676155
ironsiuv <s.ammirati95@gmail.com>
2019-07-16 12:52:09 +0200

    FIRST COMMIT! - Uploading blk template - Integrating TravisCI and Codecov

--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { FirestoreProvider } from 'react-firestore';
 import * as serviceWorker from './serviceWorker';
+require('dotenv').config();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SND_ID
+};
+
+
+firebase.initializeApp(config);
+
+
+ReactDOM.render(<FirestoreProvider firebase={firebase}>
+    <App />
+</FirestoreProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
