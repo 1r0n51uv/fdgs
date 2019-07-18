a3b1031339e012db2090b298a77f856d88558534
ironsiuv <s.ammirati95@gmail.com>
2019-07-18 16:49:07 +0200

    - Trying to fix testing3

--- src/App.test.js
+++ src/App.test.js
@@ -6,21 +6,21 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SND_ID,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SND_ID,
 };
 
 firebase.initializeApp(config);
 
-it('renders without crashing', async () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-      <FirestoreProvider firebase={firebase}>
-    <App />
-  </FirestoreProvider>, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<FirestoreProvider firebase={firebase}>
+        <App />
+    </FirestoreProvider>, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
